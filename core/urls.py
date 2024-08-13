9b55ddd6524979d64d9331c26fa4341dd8fe6cf6
sayyUkhty <160317446+sayyUkhty@users.noreply.github.com>
2024-08-13 09:37:36 +0500

    git push origin main

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import JsonResponse
-from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
 
 urlpatterns = [
     path("admin/", admin.site.urls),
