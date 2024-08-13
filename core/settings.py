0adc1c73235afedb2f8996337ced6c52d6d14d24
sayyUkhty <160317446+sayyUkhty@users.noreply.github.com>
2024-08-13 09:35:03 +0500

    git push origin main

--- core/settings.py
+++ core/settings.py
@@ -99,7 +99,7 @@ USE_TZ = True
 
 
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / "static"
 
--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.http import JsonResponse
 from django.contrib import admin
 from django.urls import path
-from django.http import JsonResponse
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('health/', lambda _: JsonResponse({'detail': 'Healthy'}), name='health'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
