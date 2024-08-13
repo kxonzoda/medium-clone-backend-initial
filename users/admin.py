08641dd72a3cf602de218dbe0f8cd8f6ccdd175d
sayyUkhty <160317446+sayyUkhty@users.noreply.github.com>
2024-08-13 15:05:38 +0500

    git push origin main

--- users/admin.py
+++ users/admin.py
@@ -11,3 +11,10 @@ class CustomUserAdmin(UserAdmin):
     list_display_links = ('id', 'username', 'email')
     search_fields = ('username', 'email', 'first_name', 'last_name', 'middle_name')
     list_filter = ('last_login', 'date_joined', 'is_staff', 'is_superuser', 'is_active')
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional info', {
+            'fields': ('middle_name', 'avatar',)
+        }),
+    )
