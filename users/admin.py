3c8c5165c27886aa6be01c3242b8892010e93a14
sayyUkhty <160317446+sayyUkhty@users.noreply.github.com>
2024-08-13 15:17:55 +0500

    git push origin main

--- users/admin.py
+++ users/admin.py
@@ -5,16 +5,12 @@ from .models import CustomUser
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):  
     fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('middle_name',)}),
+        ('Additional info', {
+            'fields': ('middle_name', 'avatar',)
+        }),
     )
     list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'middle_name')
     list_display_links = ('id', 'username', 'email')
     search_fields = ('username', 'email', 'first_name', 'last_name', 'middle_name')
     list_filter = ('last_login', 'date_joined', 'is_staff', 'is_superuser', 'is_active')
 
-class CustomUserAdmin(UserAdmin):
-    fieldsets = UserAdmin.fieldsets + (
-        ('Additional info', {
-            'fields': ('middle_name', 'avatar',)
-        }),
-    )
